d47218feb927663341282c9b818e5792a0213315
Armando Júnior <armandosvjunior@yahoo.com.br>
2013-04-14 19:13:02 -0300

    Implementação dos métodos de busca.

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -1,7 +1,63 @@
 package controllers;
 
+import java.util.List;
+
+import models.Category;
 import models.Product;
 
+class ProductByCategoryCollector extends Collector<Product>{
+	private Category category;
+	
+	public ProductByCategoryCollector(Category category){
+		this.setCategory(category);
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	public boolean collect(Product model){
+		return (model.getCategory().equals(this.getCategory()));
+	}
+}
+
+class ProductByNameCollector extends Collector<Product>{
+	private String search;
+
+	public ProductByNameCollector(String search)
+	{
+		this.setSearch(search);
+	}
+
+	public String getSearch()
+	{
+		return this.search;
+	}
+
+	public void setSearch(String search)
+	{
+		this.search = search;
+	}
+
+	public boolean collect(Product model){
+		return model.getName().contains(this.getSearch());
+	}
+}
+
 public class ProductController extends Controller<Product> {
+	
+	public List<Product> collectByCategory(Category category)
+	{
+		return this.collect(new ProductByCategoryCollector(category));
+	}
+	
+	public List<Product> collectByName(String search)
+	{
+		return this.collect(new ProductByNameCollector(search));
+	}
 	
 }
